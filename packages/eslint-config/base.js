db025c846a58180f5eb5fc5916bf663b481680dc
hibohiboo <hibohiboo66+github@gmail.com>
2026-03-24 07:45:18 +0900

    add sonarjs

--- packages/eslint-config/base.js
+++ packages/eslint-config/base.js
@@ -2,8 +2,10 @@ import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import { defineConfig } from "eslint/config";
+import sonarjs from 'eslint-plugin-sonarjs';
 
 export const baseConfig = defineConfig([
+  { ...sonarjs.configs.recommended, files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"] },
   { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
   tseslint.configs.recommended,
 ]);
