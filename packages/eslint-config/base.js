b3983b1c9d085883c905cbaeb031d4cc22509f5b
hibohiboo <hibohiboo66+github@gmail.com>
2026-03-24 18:56:51 +0900

    add prettier

--- packages/eslint-config/base.js
+++ packages/eslint-config/base.js
@@ -3,9 +3,11 @@ import globals from "globals";
 import tseslint from "typescript-eslint";
 import { defineConfig } from "eslint/config";
 import sonarjs from 'eslint-plugin-sonarjs';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
 
 export const baseConfig = defineConfig([
   { ...sonarjs.configs.recommended, files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"] },
   { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
   tseslint.configs.recommended,
+  eslintConfigPrettier,
 ]);
