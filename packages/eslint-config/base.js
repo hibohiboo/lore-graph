6bb93b788c4cf2adedad4875e88b3045e7b51cf9
hibohiboo <hibohiboo66+github@gmail.com>
2026-03-24 23:48:46 +0900

    add unused imports

--- packages/eslint-config/base.js
+++ packages/eslint-config/base.js
@@ -4,10 +4,11 @@ import tseslint from "typescript-eslint";
 import { defineConfig } from "eslint/config";
 import sonarjs from 'eslint-plugin-sonarjs';
 import eslintConfigPrettier from 'eslint-config-prettier/flat';
+import unusedImports from 'eslint-plugin-unused-imports';
 
 export const baseConfig = defineConfig([
   { ...sonarjs.configs.recommended, files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"] },
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], plugins: { js,'unused-imports': unusedImports  }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
   tseslint.configs.recommended,
   eslintConfigPrettier,
 ]);
