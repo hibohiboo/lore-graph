6ac8fc72fc62709a4986fca66a7adffe200c73b1
hibohiboo <hibohiboo66+github@gmail.com>
2026-03-25 08:40:55 +0900

    add prettier

--- packages/eslint-config/base.js
+++ packages/eslint-config/base.js
@@ -1,15 +1,23 @@
-import js from "@eslint/js";
-import globals from "globals";
-import tseslint from "typescript-eslint";
-import { defineConfig } from "eslint/config";
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import { defineConfig } from 'eslint/config';
 import sonarjs from 'eslint-plugin-sonarjs';
 import eslintConfigPrettier from 'eslint-config-prettier/flat';
 import unusedImports from 'eslint-plugin-unused-imports';
 
 export const baseConfig = defineConfig([
-  { ...sonarjs.configs.recommended, files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"] },
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], plugins: { js,'unused-imports': unusedImports  }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  {
+    ...sonarjs.configs.recommended,
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+  },
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    plugins: { js, 'unused-imports': unusedImports },
+    extends: ['js/recommended'],
+    languageOptions: { globals: globals.browser },
+  },
   tseslint.configs.recommended,
   eslintConfigPrettier,
 ]);
-export default baseConfig
+export default baseConfig;
